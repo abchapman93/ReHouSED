fd0cb36e78273b242403fd59677946c91200d995
Alec Chapman <abchapman93@gmail.com>
2024-02-18 08:17:44 -0700

    adding package resources

--- setup.py
+++ setup.py
@@ -9,6 +9,17 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
+# function to recursively get files for resourcee
+def package_files(directory):
+    paths = []
+    for (p, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join("..", p, filename))
+    return paths
+
+# get all files recursively from /resources
+resource_files = package_files("./rehoused_nlp/resources")
+
 def get_version():
     """Load the version from version.py, without importing it.
     This function assumes that the last line in the file contains a variable defining the
@@ -28,8 +39,10 @@ setup(
     author_email="alec.chapman@hsc.utah.edu",
     packages=find_packages(),
     install_requires=[
-        "medspacy==0.2.0.1",
+        "medspacy==1.1.5",
     ],
+    package_data={"rehoused_nlp": resource_files},
+    include_package_data=True,
     long_description=long_description,
     long_description_content_type="text/markdown",
 )
